e1115e3a3ceba0581d77fb4a0adb9739ddffca19
FRCSola <sola.dugbo@barstowsschool.org>
2019-02-17 23:04:11 -0600

    Formating Changes

--- DeepSpace2019/src/main/java/com/frcteam1939/deepspace2019/robot/commands/elevator/TuneElevatorPID.java
+++ DeepSpace2019/src/main/java/com/frcteam1939/deepspace2019/robot/commands/elevator/TuneElevatorPID.java
@@ -9,7 +9,6 @@ package com.frcteam1939.deepspace2019.robot.commands.elevator;
 
 import com.frcteam1939.deepspace2019.robot.Robot;
 import com.revrobotics.ControlType;
-
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -18,13 +17,11 @@ public class TuneElevatorPID extends Command {
     requires(Robot.elevator);
   }
 
-  // Called just before this Command runs the first time
   @Override
   protected void initialize() {
   
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
  
@@ -82,31 +79,22 @@ public class TuneElevatorPID extends Command {
       processVariable = Robot.elevator.sparkEncoder.getVelocity();
     } else {
       setPoint = SmartDashboard.getNumber("Set Position", 0);
-      /**
-       * As with other PID modes, Smart Motion is set by calling the
-       * setReference method on an existing pid object and setting
-       * the control type to kSmartMotion
-       */
+      
       Robot.elevator.sparkPID.setReference(setPoint, ControlType.kSmartMotion);
       processVariable = Robot.elevator.sparkEncoder.getPosition();
     }
     
-
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return false;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
   }
